fbda130c1aa306532cbc796162e5a652aa96f225
Allen <allen@darwynn.com>
2026-02-20 15:25:09 -0500

    Convert project to Java Maven (Spring Boot) structure

---
+++ src/main/java/com/couponsite/CouponSiteApplication.java
@@ -0,0 +1,12 @@
+package com.couponsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponSiteApplication.class, args);
+    }
+}
